3861cd7e180289911de5ac9a036ea37417a9445d
ahdernasr <44983175+ahdernasr@users.noreply.github.com>
2021-04-29 03:37:07 +0100

    Add flood fill + game functionalities
    TDL:
    
    Game win condition
    Reset feature

--- minesweeper.js
+++ minesweeper.js
@@ -1,3 +1,8 @@
+//TDL
+// DETETCING WIN
+// RESET 
+// DIFFICULTIES
+
 grid = document.getElementById("grid");
 rows = 14;
 cols = 18;
@@ -13,6 +18,8 @@ function createGrid() {
         x: x,
         y: y,
         bomb: false,
+        hidden: true,
+        rightClicked: false
       };
       el.classList.add("el");
       el.classList.add("hidden");
@@ -26,43 +33,81 @@ function createGrid() {
   for (e of bombsArr) {
     e.value.bomb = true;
   }
-  eventHandler();
+  eventHandler(false);
 }
 
 elements = document.getElementsByClassName("el");
+rightClicked = false;
+function eventHandler(lost) {
+  if (!lost) {
+    for (e of elements) {
+      e.addEventListener("contextmenu", rClickHandler);
 
-function eventHandler() {
-  for (e of elements) {
-    e.addEventListener("contextmenu", (event) => {
-      event.preventDefault();
-      event.target.closest(".el").classList.add("marked");
-    });
+      e.addEventListener("click", lClickHandler);
+    }
+  }
+}
 
-    e.addEventListener("click", (event) => {
-      //CHECK BOMB
+/////////////////////////STOP WATCH////////////////////
 
-      if (event.target.closest(".el").value.bomb) {
-        for (e of elements) {
-          if (e.value.bomb) {
-            e.classList.remove("hidden");
-            e.classList.add("bomb");
-          }
-        }
-      } else {
-        event.target.closest(".el").classList.remove("hidden");
+function rClickHandler(event) {
+  event.preventDefault();
+  if (!event.target.closest(".el").rightClicked && event.target.closest(".el").classList.contains('hidden')) {
+    event.target.closest(".el").classList.add("marked");
+    event.target.closest(".el").rightClicked = true;
+  } else if (event.target.closest(".el").rightClicked && event.target.closest(".el").classList.contains('hidden')) {
+    event.target.closest(".el").classList.remove("marked");
+    event.target.closest(".el").rightClicked = false;
+  }
+}
+
+stopwatch = document.getElementById("stopwatch");
+firstTime = true;
+function lClickHandler(event) {
+  reveal(event.target.closest(".el"));
+  if (checkWin()) alert("You win!");
+  if (firstTime) {
+    value = 0
+    myInterval = setInterval(() => {
+      value+=1
+      stopwatch.textContent = `${value} s`
+    }, 1000)
+  }
+  firstTime = false;
+}
+
+function reveal(el) {
+  if (el.value.bomb) {
+    for (e of elements) {
+      if (e.value.bomb) {
+        e.classList.remove("hidden");
+        e.classList.add("bomb");
       }
+    }
+    lock();
+    return;
+  } else {
+    el.classList.remove("hidden");
+    el.value.hidden = false;
+  }
 
-      //FIND ADJACENTS
-      if (!event.target.closest(".el").classList.contains("bomb")) {
-        if (findBombCount(findAdjacents(event.target.closest(".el"))) != 0) {
-          event.target.closest(".el").textContent = findBombCount(
-            findAdjacents(event.target.closest(".el"))
-          );
-        } else {
-          explode(event.target.closest(".el"));
-        }
+  adj = findAdjacents(el);
+  if (findBombCount(findAdjacents(el)) != 0) {
+    el.textContent = findBombCount(adj);
+    return;
+  }
+
+  flood(el);
+}
+
+function flood(el) {
+  adj = findAdjacents(el);
+  for (a of adj) {
+    if (a) {
+      if (!a.value.bomb && a.classList.contains("hidden")) {
+        reveal(a);
       }
-    });
+    }
   }
 }
 
@@ -82,21 +127,39 @@ function getRandom(arr, n) {
 
 function findEl(x, y) {
   for (e of elements) {
-    if (e.value.x == x && e.value.y == y) {
-      return e;
+    if (e) {
+      if (e.value.x == x && e.value.y == y) {
+        return e;
+      }
     }
   }
 }
 
+Array.prototype.remove = function () {
+  var w,
+    a = arguments,
+    L = a.length,
+    ax;
+  while (L && this.length) {
+    w = a[--L];
+    while ((ax = this.indexOf(w)) !== -1) {
+      this.splice(ax, 1);
+    }
+  }
+  return this;
+};
+
 function findAdjacents(current) {
-  left = findEl(current.value.x - 1, current.value.y + 0);
-  right = findEl(current.value.x + 1, current.value.y + 0);
-  bottom = findEl(current.value.x + 0, current.value.y + 1);
-  above = findEl(current.value.x + 0, current.value.y - 1);
-  topright = findEl(current.value.x + 1, current.value.y - 1);
-  topleft = findEl(current.value.x - 1, current.value.y - 1);
-  bottomright = findEl(current.value.x + 1, current.value.y + 1);
-  bottomleft = findEl(current.value.x - 1, current.value.y + 1);
+  if (current) {
+    left = findEl(current.value.x - 1, current.value.y + 0);
+    right = findEl(current.value.x + 1, current.value.y + 0);
+    bottom = findEl(current.value.x + 0, current.value.y + 1);
+    above = findEl(current.value.x + 0, current.value.y - 1);
+    topright = findEl(current.value.x + 1, current.value.y - 1);
+    topleft = findEl(current.value.x - 1, current.value.y - 1);
+    bottomright = findEl(current.value.x + 1, current.value.y + 1);
+    bottomleft = findEl(current.value.x - 1, current.value.y + 1);
+  }
   adjacents = [
     left,
     right,
@@ -107,13 +170,14 @@ function findAdjacents(current) {
     bottomright,
     bottomleft,
   ];
+
   return adjacents;
 }
 
 function findBombCount(adjacents) {
   bombCount = 0;
   for (a of adjacents) {
-    if (a.value) {
+    if (a && a.value) {
       if (a.value.bomb) {
         bombCount += 1;
       }
@@ -122,27 +186,24 @@ function findBombCount(adjacents) {
   return bombCount;
 }
 
-function explode(start) {
-  explodingArr = [];
-  startAdjacents = findAdjacents(start);
-  explodingArr.push(startAdjacents);
-//   while (explodingArr) {
-    for (ex of explodingArr) {
-      bombCount = 0;
-      console.log(ex.value)
-    //   newAdj = findAdjacents(ex);
-    //   for (a of newAdj) {
-    //     if (a.value.bomb) {
-    //       bombCount += 1;
-    //     } else {
-    //       a.classList.remove("hidden");
-    //     }
-    //     if (bombCount == 0) {
-    //       explodingArr.push(newAdj);
-    //     }
-    //  }
-    // }
-  } 
+function checkWin() {
+  for (e of elements) {
+    if (e.value.hidden || e.value.bomb) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function lock() {
+  console.log("locking");
+  for (e of elements) {
+    e.removeEventListener("click", lClickHandler);
+    e.removeEventListener("contextmenu", rClickHandler);
+    clearInterval(myInterval)
+  }
 }
 
 createGrid();
+
+
